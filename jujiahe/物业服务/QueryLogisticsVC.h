2235c4e5670652612163a8c08747aa177caf5b3e
XiaMingjiang <xiamingjiang@copticomm.com>
2018-05-18 11:26:52 +0800

    新增物流查询页面

---
+++ jujiahe/物业服务/QueryLogisticsVC.h
@@ -0,0 +1,13 @@
+//
+//  QueryLogisticsVC.h
+//  jujiahe
+//
+//  Created by XiaMingjiang on 2018/5/18.
+//  Copyright © 2018年 世纪之光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QueryLogisticsVC : UIViewController
+
+@end
---
+++ jujiahe/物业服务/QueryLogisticsVC.m
@@ -0,0 +1,45 @@
+//
+//  QueryLogisticsVC.m
+//  jujiahe
+//
+//  Created by XiaMingjiang on 2018/5/18.
+//  Copyright © 2018年 世纪之光. All rights reserved.
+//
+
+#import "QueryLogisticsVC.h"
+
+@interface QueryLogisticsVC ()
+
+@property (nonatomic, strong) UITextField *numberTextField;
+@property (nonatomic, strong) UIButton *queryButton;
+
+@end
+
+@implementation QueryLogisticsVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+#pragma mark - getter
+
+- (UITextField *)numberTextField
+{
+    if (!_numberTextField) {
+        _numberTextField = [[UITextField alloc] init];
+    }
+    return _numberTextField;
+}
+
+- (UIView *)textFiledLeftView
+{
+    UIView *view = [[UIView alloc] initWithFrame:<#(CGRect)#>]
+    
+    UILabel *lbl = [[UILabel alloc] init];
+    lbl.font = [UIFont systemFontOfSize:14];
+    lbl.text = @"快递单号";
+    return lbl;
+}
+
+@end
